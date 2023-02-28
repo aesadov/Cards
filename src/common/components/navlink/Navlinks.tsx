51b2b1c4b59bbc7e72717cf3ccd9e681e711e356
aesadov <aesadov@gmail.com>
2023-02-28 20:25:34 +0300

    navlinks styles refactor

--- src/common/components/navlink/Navlinks.tsx
+++ src/common/components/navlink/Navlinks.tsx
@@ -1,33 +1,34 @@
 import React from 'react';
-import {NavLink} from 'react-router-dom'
+import {NavLink} from 'react-router-dom';
 import s from './Navlihks.module.css'
+
 
 export const Navlinks = () => {
     return (
         <nav style={{position: 'absolute', bottom: '0', left: '20px'}}>
             <ul className={s.body}>
                 <li><NavLink to='/login' className={navData => navData.isActive ? s.active : s.item}>
-                    Login_
+                    Login
                 </NavLink>
                 </li>
                 <li><NavLink to='/registration' className={navData => navData.isActive ? s.active : s.item}>
-                    Registration_
+                    Registration
                 </NavLink>
                 </li>
                 <li><NavLink to='/set-new-password' className={navData => navData.isActive ? s.active : s.item}>
-                    New Password_
+                    New password
                 </NavLink>
                 </li>
                 <li><NavLink to='/forgot' className={navData => navData.isActive ? s.active : s.item}>
-                    Forgot Password_
+                    Forgot password
                 </NavLink>
                 </li>
                 <li><NavLink to='/profile' className={navData => navData.isActive ? s.active : s.item}>
-                    Profile_
+                    Profile
                 </NavLink>
                 </li>
                 <li><NavLink to='/packs' className={navData => navData.isActive ? s.active : s.item}>
-                    Packs_
+                    Packs
                 </NavLink>
                 </li>
                 <li><NavLink to='/cards' className={navData => navData.isActive ? s.active : s.item}>
